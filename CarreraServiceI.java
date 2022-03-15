ed91406d9eaf8ea4f05c1cc030d0c5b638a79138
KARINA CANCINO <92641519+karicancino@users.noreply.github.com>
2022-03-15 15:55:11 -0600

    Update CarreraServiceI.java

--- CarreraServiceI.java
+++ CarreraServiceI.java
@@ -1,13 +1,19 @@
 package com.api.uptap.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.api.uptap.entity.Carrera;
 
 public interface CarreraServiceI {
 	
-	List<Carrera> consultarCarrera();  	
+	List<Carrera> consultarCarrera();
+	Optional<Carrera> consultarCarrera(int idcarr);
+	
 	void actualizarCarrera(Carrera obj,int idcarr);
 	void registroCarrera(Carrera obj);	
 	void eliminarCarrera(int idcarr);
+	
 
 }
+
