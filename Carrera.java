ad9dbb1c2d7297d465e20f426c9bd9ba6e127822
KARINA CANCINO <92641519+karicancino@users.noreply.github.com>
2022-03-16 16:38:39 -0600

    Update Carrera.java

--- Carrera.java
+++ Carrera.java
@@ -1,17 +1,25 @@
 package com.api.uptap.entity;
 
+import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
 @Table(name = "carrera")
-
-public class Carrera {
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) 
+public class Carrera implements Serializable{
+	
+	private static final long serialVersionUID=1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +28,19 @@ public class Carrera {
 	
 	@Column(name = "nombre")
 	private String nombre;
+	
+	@OneToMany(mappedBy="id_carrerafk", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	private Set<Alumno> alumnos;
+	
+
+	
+	public Set<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(Set<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
 
 	//GENERACION AUTOMATICA DE GETTERS Y SETTERS
 	public int getId_carrera() {
@@ -37,9 +58,5 @@ public class Carrera {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	
-	
-	
-	
+
 }
