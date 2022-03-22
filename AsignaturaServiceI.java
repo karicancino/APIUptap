9f5af59d8d501edca3e11ec944bdc55f31234fca
KARINA CANCINO <92641519+karicancino@users.noreply.github.com>
2022-03-22 15:46:25 -0600

    Create AsignaturaServiceI.java

---
+++ AsignaturaServiceI.java
@@ -0,0 +1,10 @@
+package com.api.uptap.service;
+
+import java.util.List;
+import com.api.uptap.entity.Asignatura;
+
+public interface AsignaturaServiceI {
+	
+	List<Asignatura> consultarAsignatura();
+
+}
