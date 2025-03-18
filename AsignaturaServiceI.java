b2da46e2eedfaa2d3a6792e97037d2414513bf36
KARINA CANCINO <92641519+karicancino@users.noreply.github.com>
2025-03-18 14:22:55 -0600

    Update AsignaturaServiceI.java

--- AsignaturaServiceI.java
+++ AsignaturaServiceI.java
@@ -1,10 +1,17 @@
 package com.api.uptap.service;
-
 import java.util.List;
+import java.util.Optional;
+
 import com.api.uptap.entity.Asignatura;
 
 public interface AsignaturaServiceI {
-	
 	List<Asignatura> consultarAsignatura();
+	void registroAsignatura(Asignatura obj);
+	public void actualizarAsignatura(Asignatura obj, int idasig);
+	
+	void eliminarAsignatura(int idasig);
+	Optional<Asignatura> consultarAsignatura(int idasig);
 
+	
+	
 }
