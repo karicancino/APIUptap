f6bf1b34adbeec8e832841fcd8aa027998922f9d
KARINA CANCINO <92641519+karicancino@users.noreply.github.com>
2025-03-18 14:23:25 -0600

    Update AsignaturaService.java

--- AsignaturaService.java
+++ AsignaturaService.java
@@ -1,6 +1,7 @@
 package com.api.uptap.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,31 @@ public class AsignaturaService implements AsignaturaServiceI{
 
 	}
 	
+	@Override
+	public void registroAsignatura(Asignatura obj) {
+		// TODO Auto-generated method stub
+		repasig.save(obj);
+		
+	}
+	
+	@Override
+	public void actualizarAsignatura(Asignatura obj, int idasig) {
+		// TODO Auto-generated method stub
+		obj.setIdasig(idasig);
+		repasig.save(obj);  //SpringDataJPA
+	}
+	
+	@Override
+	public void eliminarAsignatura(int idasig) {
+		// TODO Auto-generated method stub
+		repasig.deleteById(idasig); //SpringDataJPA		
+	}
+
+	
+	@Override
+	public Optional<Asignatura> consultarAsignatura(int idasig) {
+		// TODO Auto-generated method stub
+		return repasig.findById(idasig);
+	}
 
 }
